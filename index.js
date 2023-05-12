d9c589276a7f4fab8aab14be04ac5b8c194cba38
mirnaiem <mirnaiem8teen@gmail.com>
2023-05-12 12:31:12 +0600

    loaded chef info

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
 const express = require('express')
+const chef=require('./chef.json')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/chefs',(req,res)=>{
+  
+  res.send(chef)
 })
 
 app.listen(port, () => {
